7b9a4b5ef9dcb5af6eb9908d000f57069cb29bc8
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2017-08-05 16:19:01 -0400

    Added file to fix MonoDevelop .NET version

---
+++ Assets/Editor/ChangeNetVersion.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lemmings.Editor {
+
+    /// <summary>
+    /// Replaces the unavailable .NET 4.6 version for .csproj files with an available one (4.5.1).
+    /// </summary>
+    class ChangeNetVersion : UnityEditor.AssetModificationProcessor {
+
+        /// <summary>
+        /// Called when assets are saved.
+        /// </summary>
+        /// <param name="paths">The paths of saved assets.</param>
+        static string[] OnWillSaveAssets(string[] paths) {
+            foreach (string fileName in new string[]{"Assembly-CSharp.csproj", "Assembly-CSharp-Editor.csproj"}) {
+                ReplaceNetString(fileName);
+            }
+            return paths;
+        }
+
+        /// <summary>
+        /// Replaces the unavailable .NET 4.6 version for a .csproj file with an available one (4.5.1).
+        /// </summary>
+        /// <param name="fileName">The file path of the .csproj file.</param>
+        private static void ReplaceNetString(string fileName) {
+            string text = File.ReadAllText(fileName);
+            text = text.Replace("v4.6", "v4.5.1");
+            File.WriteAllText(fileName, text);
+        }
+    }
+}
